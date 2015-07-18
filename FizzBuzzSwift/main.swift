4533ae230b9397d2bdbdf51d41f5f07b1569cbf0
Franck Horlaville <fh@tamsoftware.info>
2015-07-18 21:48:46 +0100

    Initial Commit

---
+++ FizzBuzzSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FizzBuzzSwift
+//
+//  Created by Franck Horlaville on 18/07/2015.
+//  Copyright (c) 2015 TAM Software. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
